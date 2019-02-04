f9293d39b49ec41bbb0f67077558541896b03e91
hello <hello@hellos-iMac.local>
2019-02-04 18:31:30 +0100

    Handling user input

--- app/javascript/components/AppointmentForm.js
+++ app/javascript/components/AppointmentForm.js
@@ -1,18 +1,43 @@
 import React from "react"
 import PropTypes from "prop-types"
 class AppointmentForm extends React.Component {
+  handleChange = e => {
+    const { onUserInput } = this.props;
+    onUserInput({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render () {
+    const { input_title, input_appt_time } = this.props;
+    const { handleChange } = this;
     return (
       <React.Fragment>
         <h2>Make a new appointment</h2>
         <form>
-          <input name="title" placeholder="Appointment title" />
-          <input name="appt_time" placeholder="Date and Time" />
+          <input
+            name="input_title"
+            placeholder="Appointment title"
+            value={input_title}
+            onChange={handleChange}
+          />
+          <input
+            name="input_appt_time"
+            placeholder="Date and Time"
+            value={input_appt_time}
+            onChange={handleChange}
+          />
           <input type="submit" value="Make appointment" />
         </form>
       </React.Fragment>
     );
   }
 }
+
+AppointmentForm.propTypes = {
+  input_title: PropTypes.string,
+  input_appt_time: PropTypes.string,
+  onUserInput: PropTypes.func,
+};
 
 export default AppointmentForm
--- app/javascript/components/Appointments.js
+++ app/javascript/components/Appointments.js
@@ -3,11 +3,33 @@ import PropTypes from "prop-types"
 import AppointmentForm from "./AppointmentForm";
 import AppointmentList from "./AppointmentList";
 class Appointments extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      appointments: props.appointments,
+      input_title: 'Team startup meeting',
+      input_appt_time: 'Tomorrow at 9am',
+    };
+  }
+
+  handleUserInput = obj => {
+    this.setState(prevState => ({
+      ...prevState,
+      ...obj,
+    }));
+  };
+
   render () {
-    const { appointments } = this.props;
+    const { appointments, input_title, input_appt_time } = this.state;
+    const { handleUserInput } = this;
     return (
       <React.Fragment>
-        <AppointmentForm />
+        <AppointmentForm
+          input_title={input_title}
+          input_appt_time={input_appt_time}
+          onUserInput={handleUserInput}
+        />
         <AppointmentList appointments={appointments} />
       </React.Fragment>
     );
