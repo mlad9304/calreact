3df256131fc3b3aa0e2072ba3d17582ecbfd8d98
hello <hello@hellos-iMac.local>
2019-02-04 15:10:01 +0100

    Create Appointments controller and react component

---
+++ app/javascript/components/Appointments.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Appointments extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Appointments
