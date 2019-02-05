7237a93c73b1b0fff396e65b4c72ed7f020069d7
hello <hello@hellos-iMac.local>
2019-02-05 09:35:44 +0100

    Configuration for deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_02_04_142431) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appointments", force: :cascade do |t|
     t.string "title"
     t.datetime "appt_time"
