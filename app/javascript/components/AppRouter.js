095777a9d1108bd6c683942c768a64e3fc972caa
hello <hello@hellos-iMac.local>
2019-02-08 10:11:44 +0100

    Add app header

--- app/javascript/components/AppRouter.js
+++ app/javascript/components/AppRouter.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Appointments from './Appointments';
 import Appointment from './Appointment';
+import AppHeader from './AppHeader';
 
 const AppRouter = (props) => {
     return (
         <Router>
-            <Switch>
+            <React.Fragment>
+                <AppHeader />
                 <Route exact path="/" render={routeProps => (
                     <Appointments {...routeProps} appointments={props.appointments} />
                 )} />
                 <Route path="/appointments/:id" component={Appointment} />
-            </Switch>
+            </React.Fragment>
         </Router>
     );
 }
