1430056e37e57670cfde87de7e2513bd62801037
rndev80 <rndev80@gmail.com>
2019-03-15 15:48:08 +0100

    Using devise with React

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,9 +1,10 @@
 class AppointmentsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
 
   def index
-    @appointments = Appointment.order('appt_time ASC')
-    @appointment = Appointment.new
+    @appointments = current_user.appointments.order('appt_time ASC')
+    @appointment = current_user.appointments.new
 
     respond_to do |format| 
       format.html 
@@ -12,7 +13,7 @@ class AppointmentsController < ApplicationController
   end
 
   def show
-    @appointment = Appointment.find(params[:id])
+    @appointment = current_user.appointments.find(params[:id])
     respond_to do |format|
       format.html { render :index }
       format.json { render json: @appointment }
@@ -24,7 +25,7 @@ class AppointmentsController < ApplicationController
   end
 
   def update
-    @appointment = Appointment.find(params[:id])
+    @appointment = current_user.appointments.find(params[:id])
     if @appointment.update(appointment_params)
       render json: @appointment
     else
@@ -33,7 +34,7 @@ class AppointmentsController < ApplicationController
   end
 
   def create
-    @appointment = Appointment.create(appointment_params)
+    @appointment = current_user.appointments.create(appointment_params)
     if @appointment.save
       render json: @appointment
     else
@@ -42,7 +43,7 @@ class AppointmentsController < ApplicationController
   end
 
   def destroy
-    @appointment = Appointment.find(params[:id])
+    @appointment = current_user.appointments.find(params[:id])
     if @appointment.destroy
       head :no_content, status: :ok
     else
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_080121) do
+ActiveRecord::Schema.define(version: 2019_03_15_143254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_02_09_080121) do
     t.datetime "appt_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_02_09_080121) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "appointments", "users"
 end
